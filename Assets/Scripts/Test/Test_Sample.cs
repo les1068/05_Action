5f83183c1b3992c0c729e5e411d1057f50db4610
les1068 <81298309+les1068@users.noreply.github.com>
2023-04-19 14:07:08 +0900

    230419-5 Action setting

---
+++ Assets/Scripts/Test/Test_Sample.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_Sample : Test_Base
+{
+    protected override void Test1(InputAction.CallbackContext _)
+    {
+        Debug.Log("1");
+    }
+}
