f2194e5057ed2cded89f69d7a73c282c8d6fe76d
les1068 <81298309+les1068@users.noreply.github.com>
2023-04-24 14:16:18 +0900

    230424-5 인벤토리(아이템 데이터 작성)

---
+++ Assets/Scripts/Editor/ItemDataEditor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+// ItemData용 ㅓ스텀 에디터라는 의미, 두번째 파라메터가 ture면 자식 클래스도 같이 적용 받는다.
+[CustomEditor(typeof(ItemData), true)]
+public class ItemDataEditor : Editor
+{
+    ItemData itemData;
+
+    private void OnEnable()
+    {
+        itemData = target as ItemData;
+    }
+    public override void OnInspectorGUI()
+    {
+        if(itemData != null) 
+        {
+            if(itemData.itemIcon != null)
+            {
+                Texture2D texture;
+                EditorGUILayout.LabelField("Item Icon Preview");
+                texture = AssetPreview.GetAssetPreview(itemData.itemIcon);
+                if (texture != null )
+                {
+                    GUILayout.Label("",GUILayout.Height(64),GUILayout.Width(64));
+                    GUI.DrawTexture(GUILayoutUtility.GetLastRect(), texture);
+                }
+            }
+        }
+        base.OnInspectorGUI();
+    }
+}
+#endif
