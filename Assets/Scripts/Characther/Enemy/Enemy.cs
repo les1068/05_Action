19fb35c8e444d3f98c0b84bd5ba2675d759eadb8
les1068 <81298309+les1068@users.noreply.github.com>
2023-05-15 10:49:37 +0900

    230515-2 슬라임 행동추적

--- Assets/Scripts/Characther/Enemy/Enemy.cs
+++ Assets/Scripts/Characther/Enemy/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
@@ -29,34 +30,48 @@ public class Enemy : MonoBehaviour
         Attack,    // 공격 상태
         Die        // 죽은 상태
     }
-    EnemyState state = EnemyState.Patrol;
+
+    /// <summary>
+    /// 현재 적의 상태
+    /// </summary>
+    EnemyState state = EnemyState.Patrol; // Start할 때 wait로 설정하기 위해 임시로 Patrol로 설정
+
+    /// <summary>
+    /// 상태를 확인하고 변경시 일어나는 처리를 하는 프로퍼티
+    /// </summary>
     EnemyState State
     {
         get => state;
         set
         {
-            if (state != value)
+            if (state != value)   // 상태가 변경될 때만 실행
             {
                 state = value;
-                switch (state)
+                switch (state)    // 변경된 상태에 따라 서로 다른 처리를 수행
                 {
                     case EnemyState.Wait:
-                        agent.isStopped = true;
-                        agent.velocity = Vector3.zero;
-                        WaitTimer = waitDuration + UnityEngine.Random.Range(0.0f, 0.5f);
-                        anim.SetTrigger("Stop");
-                        StateUpdata = Update_Wait;
+                        // Wait 상태가 될 때 처리해야 할 일들
+                        agent.isStopped = true;          // 길찾기로 움직이던 것 정지
+                        agent.velocity = Vector3.zero;   // 길찾기 관성 제거
+                        anim.SetTrigger("Stop");         // Idle 애니메이션 재생
+                        WaitTimer = waitDuration + UnityEngine.Random.Range(0.0f, 0.5f);  // 기다릴 시간 설정
+                        StateUpdate = Update_Wait;       // Wait 상태용 업데이트 함수 설정
                         break;
 
                     case EnemyState.Patrol:
-                        agent.isStopped = false;
-                        agent.SetDestination(moveTarget.position);
-                        anim.SetTrigger("Move");
-                        StateUpdata = Updata_Patrol;
+                        // Patrol 상태가 될 때 처리해야 할 일들
+                        agent.isStopped = false;         // 길찾기 정지를 해제(다시 움직일 수 있게 설정)
+                        agent.SetDestination(moveTarget.position);  // 움직일 목적지 설정
+                        anim.SetTrigger("Move");         // Move 애니메이션 재생
+                        StateUpdate = Updata_Patrol;     // Patrol 상태용 업데이트 함수 설정
                         break;
 
                     case EnemyState.Chase:
-                        
+                        // Chase 상태가 될 때 처리해야 할 일들
+                        agent.isStopped = false;         // 길찾기 정지를 해제(다시 움직일 수 있게 설정)
+                        agent.SetDestination(chaseTarget.position);  // 추적 대상으로 이동하게 설정
+                        anim.SetTrigger("Move");         // Move 애니메이션 재생
+                        StateUpdate = Updata_Chase;      // Chase 상태용 업데이트 함수 설정
                         break;
 
                     case EnemyState.Attack:
@@ -68,7 +83,11 @@ public class Enemy : MonoBehaviour
             }
         }
     }
-    Action StateUpdata = null;
+
+    /// <summary>
+    /// 상태별 Update함수를 저장할 델리게이트 
+    /// </summary>
+    Action StateUpdate = null;
 
 
     // 순찰 관련 데이터 --------------------------------
@@ -91,7 +110,7 @@ public class Enemy : MonoBehaviour
     public float waitDuration = 1.0f;
 
     /// <summary>
-    /// 실제로 기다린 시간
+    /// 실제로 기다릴 시간
     /// </summary>
     float waitTimer = 0.0f;
     float WaitTimer
@@ -100,12 +119,35 @@ public class Enemy : MonoBehaviour
         set
         {
             waitTimer = value;
-            if (waypoints != null && waitTimer < 0.0f)
+            if (waypoints != null && waitTimer < 0.0f)   // waitTimer가 0보다 작아지면 순찰 상태로 전환
             {
                 State = EnemyState.Patrol;
             }
         }
     }
+
+    // 추적 관련 데이터 --------------------------------
+
+    /// <summary>
+    /// 전체 시야 범위
+    /// </summary>
+    public float sightRange = 10.0f;
+
+    /// <summary>
+    /// 시야각의 절반
+    /// </summary>
+    public float sightHalfAngle = 50.0f;
+
+    /// <summary>
+    /// 근접 시야 범위
+    /// </summary>
+    public float closeSightRange = 2.5f;
+
+    /// <summary>
+    /// 주적할 대상의 트랜스폼
+    /// </summary>
+    Transform chaseTarget;
+
 
     // 컴포넌트 ---------------------------------------
 
@@ -118,10 +160,11 @@ public class Enemy : MonoBehaviour
         anim = GetComponent<Animator>();
 
         Waypoints defaultWaypoints = GetComponentInChildren<Waypoints>();
+
         defaultWaypoints.transform.SetParent(null);
         if (waypoints == null)
         {
-            waypoints = defaultWaypoints;
+            waypoints = defaultWaypoints; // 따로 설정한 웨이포인트가 없으면 자식으로 붙어있는 웨이포인트 사용
         }
     }
 
@@ -129,23 +172,99 @@ public class Enemy : MonoBehaviour
     {
         moveTarget = waypoints.Current;
         State = EnemyState.Wait;
-        anim.ResetTrigger("Stop");
+        anim.ResetTrigger("Stop");   // 첫 Wait 상태로 가면서 Stop 트리거가 미리 설정되는 것 방지
     }
     private void Update()
     {
-        StateUpdata();
+        StateUpdate();   // 현재 상태의 Updata 함수 실행
 
     }
+
+    /// <summary>
+    /// 순찰용 Updata 함수
+    /// </summary>
     void Updata_Patrol()
     {
-        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        if (SearchPlayer())
         {
-            moveTarget = waypoints.Next();
+            // 플레이어 발견하면 즉시 추적상태로 변경
+            State = EnemyState.Chase;
+        }
+        else if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            // 목적지에 도착했다.
+            moveTarget = waypoints.Next();   // 도착하면 다음 지점 설정해 놓고
+            State = EnemyState.Wait;         // 대기 상태로 변경
+        }
+    }
+
+    /// <summary>
+    /// 대기용 Updata 함수
+    /// </summary>
+    void Update_Wait()
+    {
+        if (SearchPlayer())
+        {
+            // 플레이어 발견하면 즉시 추적상태로 변경
+            state = EnemyState.Chase;
+        }
+        else
+        {
+            // 그냥 기다리고 있는 상황이면 
+            WaitTimer -= Time.deltaTime;         // 타이머만 계속 감소
+        }
+    }
+
+    void Updata_Chase()
+    {
+        if (SearchPlayer())
+        {
+            // 플레이어 발견하면 즉시 추적상태로 변경
+            state = EnemyState.Chase;
+        }
+        else
+        {
+            // 안보이면 대기
             State = EnemyState.Wait;
         }
     }
-    void Update_Wait()
+
+
+    bool SearchPlayer()
     {
-        WaitTimer -= Time.deltaTime;
+        bool result = false;
+        Collider[] colliders = Physics.OverlapSphere(transform.position, sightRange, LayerMask.GetMask("Player"));
+        if (colliders.Length > 0)
+        {
+            Vector3 playerPos = colliders[0].transform.position;
+            Vector3 toPlayerDir = playerPos - transform.position;
+            if (toPlayerDir.sqrMagnitude < closeSightRange * closeSightRange)
+            {
+                // 근접 시야 범위 안에 플레이어가 있다.
+                chaseTarget = colliders[0].transform;
+                result = true;
+            }
+            else
+            {
+                // 전체 시야 범위 안에 플레이어가 있다.
+                float angle = Vector3.Angle(transform.forward, toPlayerDir);
+                if (angle < sightHalfAngle)
+                {
+                    // 시야각 안에 플레이어가 있다.
+                    Ray ray = new Ray(transform.position + transform.up * 0.5f, toPlayerDir);
+                    if (Physics.Raycast(ray, out RaycastHit hit, sightRange))
+                    {
+                        // 시야에 부딪친 물체가 있다.
+                        if (hit.collider.CompareTag("Player"))
+                        {
+                            // 부딪친 물체가 플레이어이다.
+                            chaseTarget = colliders[0].transform;
+                            result = true;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
